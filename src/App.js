c68fbc796b1f8b1ca8e50ddf161b1a93e41d858b
Zhyldyz <jargynbaeva@gmail.com>
2022-04-10 18:32:48 +0600

    WeatherApi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
+/*this object will have your keys and the base urs*/
+/* please get your key from https://home.openweathermap.org/users/sign_up */
+const api = {
+  key: "a831657c4f0540ed933d86d1e45da3ee",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const cities = ["Select a City", "Bishkek", "california", "Sonsonate"];
+  {
+    /*you can add more cities here*/
+  }
+
+  // im giving you one useState that is with the start value of the cities index[1]
+  const [selectedCity, setSelectedCity] = useState(cities[1]);
+  // maybe after calling the API in use effect you will need to save the information in another useState
+  // like weather
+  const [weather, setWeather] = useState({})
+  // remeber every time the city changes you need to call the api with the new data
+  //here you will write your useEffect
+
+  /*create a fuction to call the api*/
+  /* here you have a model of the api*/
+  const callApi = async() => {
+    const req = await fetch(`${api.base}weather?q=${selectedCity}&units=metric&APPID=${api.key}`)
+    const data = await req.json()
+    console.log(data)
+  }
+  callApi()
+
+  // here is needed to filter the data that you aredy got from your API
+  //
+  // HINT !!!! maybe when you call the API the will be empty and you will have an error
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container warm">
+      <div className=" app warm">
+        <main>
+          <div className="top">
+            <div className="location">{}</div>
+            {/* render The city*/}
+            <div>
+              <div className="temp">
+                <h2>{}</h2> {/*render the temperature*/}
+              </div>
+              <div>
+                <div className="situation">
+                  <h3>{}</h3> {/*render Situation*/}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="select-area">
+            {" "}
+            {/*create a selector to show the cities
+            on change you need to update your selectedCity
+*/}
+            <select className="custom-select" value="value" onChange="onchange">
+              {/* we need to map our cities in order to show the options */}
+
+              <option></option>
+            </select>
+            <br />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
